dd3bd86c5e34ce8811d97dd45d1ebdb76be7fc28
krishpranav <krisna.pranav@gmail.com>
2021-12-09 20:36:12 +0530

    src: configure

--- src/configure.rs
+++ src/configure.rs
@@ -2,7 +2,6 @@ use std::net::Ipv4Addr;
 use pnet::util::MacAddr;
 use pnet::packet::arp::ArpOperation;
 
-
 #[derive(Debug)]
 pub struct Configure {
     pub interface: String,
@@ -10,4 +9,5 @@ pub struct Configure {
     pub source_mac: MacAddr,
     pub target_ip: Ipv4Addr,
     pub target_mac: MacAddr,
+    pub arp_operation: ArpOperation
 }
